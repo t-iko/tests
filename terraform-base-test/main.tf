1e2eadb31710f28a991f4b81da6dfecf0e201006
ikoma <live>
2019-05-18 18:08:15 +0900

    mulitaz

--- terraform-base-test/main.tf
+++ terraform-base-test/main.tf
@@ -36,47 +36,76 @@ resource "aws_internet_gateway" "terraform_test_igw" {
 ######################################
 # Subnet（Public）を作成する
 ######################################
-resource "aws_subnet" "terraform_test_public_subnet" {
+resource "aws_subnet" "terraform_test_public_subnet1a" {
   vpc_id = "${aws_vpc.terraform_test_vpc.id}"
-  cidr_block = "${var.public_subnet_cidr}"
-  availability_zone = "${var.availability_zone}"
+  cidr_block = "${var.public_subnet_cidr1}"
+  availability_zone = "${var.availability_zone1}"
 
   tags {
-    Name = "${var.public_subnet_name}"
+    Name = "${var.public_subnet_name1}"
+  }
+}
+
+resource "aws_subnet" "terraform_test_public_subnet1c" {
+  vpc_id = "${aws_vpc.terraform_test_vpc.id}"
+  cidr_block = "${var.public_subnet_cidr2}"
+  availability_zone = "${var.availability_zone2}"
+
+  tags {
+    Name = "${var.public_subnet_name2}"
   }
 }
 
 ######################################
 # Subnet（Private）を作成する
 ######################################
-resource "aws_subnet" "terraform_test_private_subnet" {
+resource "aws_subnet" "terraform_test_private_subnet1a" {
   vpc_id = "${aws_vpc.terraform_test_vpc.id}"
-  cidr_block = "${var.private_subnet_cidr}"
-  availability_zone = "${var.availability_zone}"
+  cidr_block = "${var.private_subnet_cidr1}"
+  availability_zone = "${var.availability_zone1}"
 
   tags {
-    Name = "${var.private_subnet_name}"
+    Name = "${var.private_subnet_name1}"
+  }
+}
+
+resource "aws_subnet" "terraform_test_private_subnet1c" {
+  vpc_id = "${aws_vpc.terraform_test_vpc.id}"
+  cidr_block = "${var.private_subnet_cidr2}"
+  availability_zone = "${var.availability_zone2}"
+
+  tags {
+    Name = "${var.private_subnet_name2}"
   }
 }
 
 ######################################
 # NatGatewayを作成する
 ######################################
-resource "aws_nat_gateway" "terraform_test_nat" {
-  allocation_id = "${var.eipalloc_id}"
-  subnet_id = "${aws_subnet.terraform_test_public_subnet.id}"
+resource "aws_nat_gateway" "terraform_test_nat1" {
+  allocation_id = "${var.eipalloc_id1}"
+  subnet_id = "${aws_subnet.terraform_test_public_subnet1a.id}"
 
   tags {
-    Name = "${var.nat_gateway_name}"
+    Name = "${var.nat_gateway_name1}"
+  }
+}
+
+resource "aws_nat_gateway" "terraform_test_nat2" {
+  allocation_id = "${var.eipalloc_id2}"
+  subnet_id = "${aws_subnet.terraform_test_public_subnet1c.id}"
+
+  tags {
+    Name = "${var.nat_gateway_name2}"
   }
 }
 
 ######################################
 # NetworkACL(Public)を作成する
 ######################################
-resource "aws_network_acl" "terraform_test_public_network_acl" {
+resource "aws_network_acl" "terraform_test_public_network_acl1" {
   vpc_id = "${aws_vpc.terraform_test_vpc.id}"
-  subnet_ids = ["${aws_subnet.terraform_test_public_subnet.id}"]
+  subnet_ids = ["${aws_subnet.terraform_test_public_subnet1a.id}"]
 
   egress {
     protocol   = "all"
@@ -97,16 +126,43 @@ resource "aws_network_acl" "terraform_test_public_network_acl" {
   }
 
   tags = {
-    Name = "${var.public_network_acl_name}"
+    Name = "${var.public_network_acl_name1}"
+  }
+}
+
+resource "aws_network_acl" "terraform_test_public_network_acl2" {
+  vpc_id = "${aws_vpc.terraform_test_vpc.id}"
+  subnet_ids = ["${aws_subnet.terraform_test_public_subnet1c.id}"]
+
+  egress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.public_network_acl_name2}"
   }
 }
 
 ######################################
 # NetworkACL(Private)を作成する
 ######################################
-resource "aws_network_acl" "terraform_test_private_network_acl" {
+resource "aws_network_acl" "terraform_test_private_network_acl1" {
   vpc_id = "${aws_vpc.terraform_test_vpc.id}"
-  subnet_ids = ["${aws_subnet.terraform_test_private_subnet.id}"]
+  subnet_ids = ["${aws_subnet.terraform_test_private_subnet1a.id}"]
 
   egress {
     protocol   = "all"
@@ -127,10 +183,36 @@ resource "aws_network_acl" "terraform_test_private_network_acl" {
   }
 
   tags = {
-    Name = "${var.private_network_acl_name}"
+    Name = "${var.private_network_acl_name1}"
   }
 }
 
+resource "aws_network_acl" "terraform_test_private_network_acl2" {
+  vpc_id = "${aws_vpc.terraform_test_vpc.id}"
+  subnet_ids = ["${aws_subnet.terraform_test_private_subnet1c.id}"]
+
+  egress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.private_network_acl_name2}"
+  }
+}
 ######################################
 # VPCPeeringを作成する
 ######################################
@@ -153,7 +235,7 @@ resource "aws_network_acl" "terraform_test_private_network_acl" {
 ######################################
 # RouteTable（Public）を作成する
 ######################################
-resource "aws_route_table" "terraform_test_public_route_table" {
+resource "aws_route_table" "terraform_test_public_route_table1" {
   vpc_id = "${aws_vpc.terraform_test_vpc.id}"
 
   route {
@@ -162,19 +244,32 @@ resource "aws_route_table" "terraform_test_public_route_table" {
   }
 
   tags {
-    Name = "${var.public_route_table_name}"
+    Name = "${var.public_route_table_name1}"
+  }
+}
+
+resource "aws_route_table" "terraform_test_public_route_table2" {
+  vpc_id = "${aws_vpc.terraform_test_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.terraform_test_igw.id}"
+  }
+
+  tags {
+    Name = "${var.public_route_table_name2}"
   }
 }
 
 ######################################
 # RouteTable（Private）を作成する
 ######################################
-resource "aws_route_table" "terraform_test_private_route_table" {
+resource "aws_route_table" "terraform_test_private_route_table1" {
   vpc_id = "${aws_vpc.terraform_test_vpc.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = "${aws_nat_gateway.terraform_test_nat.id}"
+    nat_gateway_id = "${aws_nat_gateway.terraform_test_nat1.id}"
   }
 
 #  route {
@@ -183,21 +278,48 @@ resource "aws_route_table" "terraform_test_private_route_table" {
 #  }
 
   tags {
-    Name = "${var.private_route_table_name}"
+    Name = "${var.private_route_table_name1}"
   }
 }
 
 
+resource "aws_route_table" "terraform_test_private_route_table2" {
+  vpc_id = "${aws_vpc.terraform_test_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.terraform_test_nat2.id}"
+  }
+
+#  route {
+#    cidr_block = "${var.peer_vpc_cidr}"
+#    gateway_id = "${aws_vpc_peering_connection.terraform_test_to_api_connection.id}"
+#  }
+
+  tags {
+    Name = "${var.private_route_table_name2}"
+  }
+}
+
 ######################################
 # SubnetRouteTableAssociationを作成する
 ######################################
-resource "aws_route_table_association" "terraform_test_public_association" {
-    subnet_id = "${aws_subnet.terraform_test_public_subnet.id}"
-    route_table_id = "${aws_route_table.terraform_test_public_route_table.id}"
+resource "aws_route_table_association" "terraform_test_public_association1" {
+    subnet_id = "${aws_subnet.terraform_test_public_subnet1a.id}"
+    route_table_id = "${aws_route_table.terraform_test_public_route_table1.id}"
 }
 
-resource "aws_route_table_association" "terraform_test_private_association" {
-    subnet_id = "${aws_subnet.terraform_test_private_subnet.id}"
-    route_table_id = "${aws_route_table.terraform_test_private_route_table.id}"
+resource "aws_route_table_association" "terraform_test_public_association2" {
+    subnet_id = "${aws_subnet.terraform_test_public_subnet1c.id}"
+    route_table_id = "${aws_route_table.terraform_test_public_route_table2.id}"
 }
 
+resource "aws_route_table_association" "terraform_test_private_association1" {
+    subnet_id = "${aws_subnet.terraform_test_private_subnet1a.id}"
+    route_table_id = "${aws_route_table.terraform_test_private_route_table1.id}"
+}
+
+resource "aws_route_table_association" "terraform_test_private_association2" {
+    subnet_id = "${aws_subnet.terraform_test_private_subnet1c.id}"
+    route_table_id = "${aws_route_table.terraform_test_private_route_table2.id}"
+}
--- terraform-base-test/variables.tf
+++ terraform-base-test/variables.tf
@@ -6,7 +6,8 @@ variable "aws_region" {
 }
 variable "region" {}
 #variable "peer_region" {}
-variable "availability_zone" {}
+variable "availability_zone1" {}
+variable "availability_zone2" {}
 variable "env" {}
 
 #####################################
@@ -22,14 +23,23 @@ variable "env" {}
 variable "vpc_name" {}
 variable "vpc_cidr" {}
 variable "igw_name" {}
-variable "eipalloc_id" {}
-variable "nat_gateway_name" {}
-variable "public_subnet_cidr" {}
-variable "private_subnet_cidr" {}
-variable "public_subnet_name" {}
-variable "private_subnet_name" {}
-variable "public_network_acl_name" {}
-variable "private_network_acl_name" {}
-variable "public_route_table_name" {}
-variable "private_route_table_name" {}
-
+variable "eipalloc_id1" {}
+variable "eipalloc_id2" {}
+variable "nat_gateway_name1" {}
+variable "nat_gateway_name2" {}
+variable "public_subnet_cidr1" {}
+variable "public_subnet_cidr2" {}
+variable "private_subnet_cidr1" {}
+variable "private_subnet_cidr2" {}
+variable "public_subnet_name1" {}
+variable "public_subnet_name2" {}
+variable "private_subnet_name1" {}
+variable "private_subnet_name2" {}
+variable "public_network_acl_name1" {}
+variable "public_network_acl_name2" {}
+variable "private_network_acl_name1" {}
+variable "private_network_acl_name2" {}
+variable "public_route_table_name1" {}
+variable "public_route_table_name2" {}
+variable "private_route_table_name1" {}
+variable "private_route_table_name2" {}
