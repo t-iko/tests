11f77a65a9aa2ac9b75793f46d8b25ddd2865eb1
ikoma <live>
2019-05-21 14:14:36 +0900

    起動テンプレート追加

--- terraform-base-test/main.tf
+++ terraform-base-test/main.tf
@@ -388,3 +388,90 @@ resource "aws_alb_listener" "terraform_test1_alb_listener" {
     type             = "forward"
   }
 }
+
+######################################
+# 起動テンプレートを作成する(変数宣言なし)
+######################################
+resource "aws_launch_template" "terraform_test1_launch_template" {
+  name = "terraform_test1_launch_template"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 8
+      volume_type = "gp2"
+    }
+  }
+
+##  capacity_reservation_specification {
+##    capacity_reservation_preference = "open"
+##  }
+
+##  credit_specification {
+##    cpu_credits = "standard"
+##  }
+
+##  disable_api_termination = true
+
+##  ebs_optimized = true
+
+##  elastic_gpu_specifications {
+##    type = "test"
+##  }
+
+##  elastic_inference_accelerator {
+##    type = "eia1.medium"
+##  }
+
+##  iam_instance_profile {
+##    name = "test"
+##  }
+
+  image_id = "ami-012b65360eb29c11c"
+
+##  instance_initiated_shutdown_behavior = "terminate"
+
+##  instance_market_options {
+##    market_type = "spot"
+##  }
+
+  instance_type = "t2.micro"
+
+##  kernel_id = "test"
+
+  key_name = "new_key_pair"
+
+##  license_specification {
+##    license_configuration_arn = "arn:aws:license-manager:eu-west-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
+##  }
+
+##  monitoring {
+##    enabled = true
+##  }
+
+##  network_interfaces {
+##    associate_public_ip_address = true
+##  }
+
+##  placement {
+##    availability_zone = "us-west-2a"
+##  }
+
+##  ram_disk_id = "test"
+
+##  vpc_security_group_ids = ["sg-12345678"]
+
+##  tag_specifications {
+##    resource_type = "instance"
+
+##    tags = {
+##      Name = "test"
+##    }
+##  }
+
+##  user_data = "${base64encode(...)}"
+}
+
+
+
